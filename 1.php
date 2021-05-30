48059dc3a08fa514f20d1116854d8f82adbe9c37
Rade00 <59159898+Rade00@users.noreply.github.com>
2021-05-31 02:13:04 +0500

    лр3
    лр3

---
+++ 1.php
@@ -0,0 +1,13 @@
+﻿<?php
+   $tek=$_GET["tek"];
+	$url = "http://lenta.ru/rss/"; 
+   $rss = simplexml_load_file($url);       
+   $schiotchik=0;
+   foreach ($rss->channel->item as $item) {
+       $schiotchik++;
+       echo '<h2>'.$item->title.'</h2>';     
+       echo '<p>'.$item->description.'</p>'; 
+       echo "<img src=\"". (string)$item->enclosure['url'][0]. "\" width='300px'>";
+       if($schiotchik==$tek)break;
+   }
+?>
